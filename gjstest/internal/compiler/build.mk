d2b1a996377288dc152066fc7fd7df69ef14621b
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-09-19 20:25:18 +1000

    Added a binary target.

--- gjstest/internal/compiler/build.mk
+++ gjstest/internal/compiler/build.mk
@@ -20,3 +20,6 @@ $(COMPILER_PACKAGE)/compiler.pb.o : $(COMPILER_PACKAGE)/compiler.pb.h $(COMPILER
 
 $(COMPILER_PACKAGE)/compiler.o : $(COMPILER_PACKAGE)/compiler.cc $(COMPILER_PACKAGE)/compiler.pb.h base/*.h strings/strutil.h file/file_utils.h
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(COMPILER_PACKAGE)/compiler.cc -o $(COMPILER_PACKAGE)/compiler.o
+
+$(COMPILER_PACKAGE)/compiler : $(COMPILER_PACKAGE)/compiler.o $(COMPILER_PACKAGE)/compiler.pb.o base/base.a strings/strutil.o file/file_utils.o
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -lpthread $^ -o $@ -lglog -lgflags -lprotobuf
