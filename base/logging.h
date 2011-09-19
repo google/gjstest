b793317c33d5db187cbf7485999b46dc5b45367d
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-09-19 19:33:10 +1000

    Replaced logging implementation with glog.

--- base/logging.h
+++ base/logging.h
@@ -1,4 +1,6 @@
-// Copyright 2010 Google
+// Copyright 2011 Google Inc. All Rights Reserved.
+// Author: jacobsa@google.com (Aaron Jacobs)
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -14,84 +16,7 @@
 #ifndef BASE_LOGGING_H_
 #define BASE_LOGGING_H_
 
-#include <assert.h>
-#include <stdlib.h>
-#include <iostream>  // NOLINT
-#include "base/macros.h"
-
-// Debug-only checking.
-#define DCHECK(condition) assert(condition)
-#define DCHECK_EQ(val1, val2) assert((val1) == (val2))
-#define DCHECK_NE(val1, val2) assert((val1) != (val2))
-#define DCHECK_LE(val1, val2) assert((val1) <= (val2))
-#define DCHECK_LT(val1, val2) assert((val1) < (val2))
-#define DCHECK_GE(val1, val2) assert((val1) >= (val2))
-#define DCHECK_GT(val1, val2) assert((val1) > (val2))
-
-// Always-on checking
-#define CHECK(x) \
-  if (!(x)) LogMessageFatal(__FILE__, __LINE__).stream() << "Check failed: " #x
-#define CHECK_LT(x, y) CHECK((x) < (y))
-#define CHECK_GT(x, y) CHECK((x) > (y))
-#define CHECK_LE(x, y) CHECK((x) <= (y))
-#define CHECK_GE(x, y) CHECK((x) >= (y))
-#define CHECK_EQ(x, y) CHECK((x) == (y))
-#define CHECK_NE(x, y) CHECK((x) != (y))
-#define CHECK_NOTNULL(x) CHECK((x) != NULL)
-
-#define LOG_INFO LogMessage(__FILE__, __LINE__)
-#define LOG_ERROR LOG_INFO
-#define LOG_WARNING LOG_INFO
-#define LOG_FATAL LogMessageFatal(__FILE__, __LINE__)
-#define LOG_QFATAL LOG_FATAL
-
-#define VLOG(x) if ((x) <= 0) LOG_INFO.stream()
-
-#ifdef NDEBUG
-#define DEBUG_MODE 0
-#define LOG_DFATAL LOG_ERROR
-#else
-#define DEBUG_MODE 1
-#define LOG_DFATAL LOG_FATAL
-#endif
-
-#define LOG(severity) LOG_ ## severity.stream()
-#define LG LOG_INFO.stream()
-
-namespace gjstest {
-class DateLogger {
- public:
-  DateLogger();
-  char* const HumanDate();
- private:
-  char buffer_[9];
-};
-}  // namespace gjstest
-
-class LogMessage {
- public:
-  LogMessage(const char* file, int line) {
-    std::cerr << "[" << pretty_date_.HumanDate() << "] "
-              << file << ":" << line << ": ";
-  }
-  ~LogMessage() { std::cerr << "\n"; }
-  std::ostream& stream() { return std::cerr; }
-
- private:
-  gjstest::DateLogger pretty_date_;
-  DISALLOW_COPY_AND_ASSIGN(LogMessage);
-};
-
-class LogMessageFatal : public LogMessage {
- public:
-  LogMessageFatal(const char* file, int line)
-    : LogMessage(file, line) { }
-  ~LogMessageFatal() {
-    std::cerr << "\n";
-    abort();
-  }
- private:
-  DISALLOW_COPY_AND_ASSIGN(LogMessageFatal);
-};
+// Redirect to glog.
+#include <glog/logging.h>
 
 #endif  // BASE_LOGGING_H_
