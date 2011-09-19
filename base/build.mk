4217c61a24ad8aebd91ffb7d2b23fb4b80c11cba
Aaron Jacobs <jacobsa@google.com>
2011-09-19 16:18:26 +1000

    Replaced much of base/ with files from the Common Language Detector:
        http://src.chromium.org/svn/trunk/src/third_party/cld/

--- base/build.mk
+++ base/build.mk
@@ -7,9 +7,6 @@ BASE_PACKAGE = base
 base/callback.o: base/callback.cc base/*.h
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c base/callback.cc -o $@
 
-base/logging.o: base/logging.cc base/*.h
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c base/logging.cc -o $@
-
 base/stringprintf.o: base/stringprintf.cc base/*.h
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c base/stringprintf.cc -o $@
 
@@ -17,5 +14,5 @@ base/timer.o: base/timer.cc base/*.h
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c base/timer.cc -o $@
 
 
-base/base.a: base/callback.o base/logging.o base/stringprintf.o base/timer.o
+base/base.a: base/callback.o base/stringprintf.o base/timer.o
 	$(AR) $(ARFLAGS) $@ $^
