408ae1709f920134dc269fea4be51cffeb8751b4
Aaron Jacobs <jacobsa@google.com>
2011-09-19 14:18:59 +1000

    Imported internal/assertions/*.js.

---
+++ gjstest/internal/assertions/expect_that.js
@@ -0,0 +1,60 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+// Author: jacobsa@google.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal implementation of expectThat.
+
+/**
+ * Verify an expectation, reporting failure when appropriate.
+ *
+ * @param {*} obj
+ *     The object that should satisfy the supplied predicate.
+ *
+ * @param {!gjstest.Matcher} matcher
+ *     A matcher that the supplied object should satisfy.
+ *
+ * @param {function(*):!string} stringify
+ *     A function that turns arbitrary objects into a convenient human-readable
+ *     form.
+ *
+ * @param {function(string)} reportFailure
+ *     A function that will be called with a descriptive error message in the
+ *     event of failure.
+ *
+ */
+gjstest.internal.expectThat = function(
+    obj,
+    matcher,
+    stringify,
+    reportFailure) {
+  // Ask the matcher about the object.
+  var predicateResult = matcher.predicate(obj);
+
+  // If the matcher says the object is okay, we're done.
+  if (predicateResult === true) return;
+
+  // Describe the failure.
+  var failureMessage = '';
+
+  failureMessage += 'Expected: ' + matcher.description + '\n';
+  failureMessage += 'Actual:   ' + stringify(obj);
+
+  // Add a clause like "which has length 7" if one is available.
+  if (typeof(predicateResult) == 'string') {
+    failureMessage += ', ' + predicateResult;
+  }
+
+  // Report the failure.
+  reportFailure(failureMessage);
+};
---
+++ gjstest/internal/assertions/expect_that_test.js
@@ -0,0 +1,79 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+// Author: jacobsa@google.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var internalExpectThat = gjstest.internal.expectThat;
+
+function ExpectThatTest() {
+  this.predicate_ = gjstest.createMockFunction();
+  this.matcher_ = new gjstest.Matcher('desc', 'negative desc', this.predicate_);
+
+  this.stringify_ = gjstest.createMockFunction();
+  this.reportFailure_ = gjstest.createMockFunction();
+
+  // Ignore calls to stringify by default.
+  expectCall(this.stringify_)(_)
+    .willRepeatedly(returnWith(''));
+}
+registerTestSuite(ExpectThatTest);
+
+ExpectThatTest.prototype.callsMatcher = function() {
+  var obj = {};
+
+  expectCall(this.predicate_)(obj)
+    .willOnce(returnWith(true));
+
+  internalExpectThat(obj, this.matcher_, this.stringify_, this.reportFailure_);
+};
+
+ExpectThatTest.prototype.matcherSaysYes = function() {
+  var obj = {};
+
+  expectCall(this.predicate_)(_)
+    .willOnce(returnWith(true));
+
+  expectCall(this.reportFailure_)(_)
+    .times(0);
+
+  internalExpectThat(obj, this.matcher_, this.stringify_, this.reportFailure_);
+};
+
+ExpectThatTest.prototype.matcherSaysNo = function() {
+  var obj = {};
+
+  expectCall(this.predicate_)(_)
+    .willOnce(returnWith(false));
+
+  expectCall(this.stringify_)(obj)
+    .willOnce(returnWith('burrito'));
+
+  expectCall(this.reportFailure_)('Expected: desc\nActual:   burrito');
+
+  internalExpectThat(obj, this.matcher_, this.stringify_, this.reportFailure_);
+};
+
+ExpectThatTest.prototype.matcherReturnsString = function() {
+  var obj = {};
+
+  expectCall(this.predicate_)(_)
+    .willOnce(returnWith('which has too few tacos'));
+
+  expectCall(this.stringify_)(obj)
+    .willOnce(returnWith('burrito'));
+
+  expectCall(this.reportFailure_)(
+      'Expected: desc\nActual:   burrito, which has too few tacos');
+
+  internalExpectThat(obj, this.matcher_, this.stringify_, this.reportFailure_);
+};
