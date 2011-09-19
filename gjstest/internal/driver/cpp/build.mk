53d80f3a826771db14bfd38b164dbf0ace009fd3
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-09-19 20:29:04 +1000

    Fixed a link error.

--- gjstest/internal/driver/cpp/build.mk
+++ gjstest/internal/driver/cpp/build.mk
@@ -18,4 +18,4 @@ $(CPP_PACKAGE)/v8_utils_test.o : $(CPP_PACKAGE)/v8_utils_test.cc $(CPP_PACKAGE)/
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(CPP_PACKAGE)/v8_utils_test.cc -o $(CPP_PACKAGE)/v8_utils_test.o
 
 $(CPP_PACKAGE)/v8_utils_test : $(CPP_PACKAGE)/v8_utils.o $(CPP_PACKAGE)/v8_utils_test.o third_party/gmock/gmock_main.a base/base.a $(V8_DIR)/libv8.a
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -lpthread $^ -o $@
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -lpthread $^ -o $@ -lglog
