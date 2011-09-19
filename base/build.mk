964662c845de134f60390ad2100ceb88caa8e24c
Aaron Jacobs <jacobsa@google.com>
2011-09-19 12:28:26 +1000

    Added timer to the base library.

--- base/build.mk
+++ base/build.mk
@@ -13,5 +13,9 @@ base/logging.o: base/logging.cc base/*.h
 base/stringprintf.o: base/stringprintf.cc base/*.h
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c base/stringprintf.cc -o $@
 
-base/base.a: base/callback.o base/logging.o base/stringprintf.o
+base/timer.o: base/timer.cc base/*.h
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c base/timer.cc -o $@
+
+
+base/base.a: base/callback.o base/logging.o base/stringprintf.o base/timer.o
 	$(AR) $(ARFLAGS) $@ $^
