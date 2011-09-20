3b33ce5a426f8df3defda3ae111e787acf67df67
Aaron Jacobs <jacobsa@google.com>
2011-09-20 13:32:15 +1000

    Fixed dependency problems.

--- gjstest/internal/driver/cpp/build.mk
+++ gjstest/internal/driver/cpp/build.mk
@@ -8,6 +8,9 @@ COMPILER_PKG := gjstest/internal/compiler
 $(CPP_PKG)/driver.o : $(CPP_PKG)/driver.h $(CPP_PKG)/driver.cc base/*.h $(V8_DIR)/include/*.h $(COMPILER_PKG)/compiler.pb.h $(CPP_PKG)/test_case.h $(CPP_PKG)/v8_utils.h webutil/xml/xml_writer.h util/hash/hash.h third_party/cityhash/*.h strings/strutil.h util/gtl/map-util.h
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(CPP_PKG)/driver.cc -o $(CPP_PKG)/driver.o
 
+$(CPP_PKG)/test_case.o : $(CPP_PKG)/test_case.h $(CPP_PKG)/test_case.cc base/*.h $(V8_DIR)/include/*.h $(CPP_PKG)/v8_utils.h
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(CPP_PKG)/test_case.cc -o $(CPP_PKG)/test_case.o
+
 $(CPP_PKG)/v8_utils.o : $(CPP_PKG)/v8_utils.h $(CPP_PKG)/v8_utils.cc base/*.h $(V8_DIR)/include/*.h
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(CPP_PKG)/v8_utils.cc -o $(CPP_PKG)/v8_utils.o
 
