6e81a0708f04d01bdccb8f2c27398d8d91095060
Aaron Jacobs <jacobsa@google.com>
2011-09-20 14:41:37 +1000

    Removed old function.

--- gjstest/public/matchers/build.mk
+++ gjstest/public/matchers/build.mk
@@ -4,13 +4,6 @@ MATCHERS_PKG := gjstest/public/matchers
 # Tests
 ######################################################
 
-define add_js_test
-$(1) : $(1).js $(JS_TEST_DEPS) gjstest/internal/driver/cpp/driver
-	gjstest/internal/driver/cpp/driver --js_files=`echo "$(JS_TEST_DEPS) $(1).js" | perl -i -pe 's: :,:g'`
-
-JS_TESTS += $(1)
-endef
-
 $(eval $(call add_js_test,$(MATCHERS_PKG)/array_matchers_test))
 $(eval $(call add_js_test,$(MATCHERS_PKG)/boolean_matchers_test))
 $(eval $(call add_js_test,$(MATCHERS_PKG)/equality_matchers_test))
