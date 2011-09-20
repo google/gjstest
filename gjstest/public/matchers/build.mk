e05256be7ee754357e0304e6f888d0c8dc389e79
Aaron Jacobs <jacobsa@google.com>
2011-09-20 14:38:03 +1000

    Cleaned up more.

--- gjstest/public/matchers/build.mk
+++ gjstest/public/matchers/build.mk
@@ -5,15 +5,15 @@ MATCHERS_PKG := gjstest/public/matchers
 ######################################################
 
 define add_js_test
-$(MATCHERS_PKG)/$(1) : $(MATCHERS_PKG)/$(1).js $(JS_TEST_DEPS) gjstest/internal/driver/cpp/driver
-	gjstest/internal/driver/cpp/driver --js_files=`echo "$(JS_TEST_DEPS) $(MATCHERS_PKG)/$(1).js" | perl -i -pe 's: :,:g'`
+$(1) : $(1).js $(JS_TEST_DEPS) gjstest/internal/driver/cpp/driver
+	gjstest/internal/driver/cpp/driver --js_files=`echo "$(JS_TEST_DEPS) $(1).js" | perl -i -pe 's: :,:g'`
 
-JS_TESTS += $(MATCHERS_PKG)/$(1)
+JS_TESTS += $(1)
 endef
 
-$(eval $(call add_js_test,array_matchers_test))
-$(eval $(call add_js_test,boolean_matchers_test))
-$(eval $(call add_js_test,equality_matchers_test))
-$(eval $(call add_js_test,function_matchers_test))
-$(eval $(call add_js_test,number_matchers_test))
-$(eval $(call add_js_test,string_matchers_test))
+$(eval $(call add_js_test,gjstest/public/matchers/array_matchers_test))
+$(eval $(call add_js_test,gjstest/public/matchers/boolean_matchers_test))
+$(eval $(call add_js_test,gjstest/public/matchers/equality_matchers_test))
+$(eval $(call add_js_test,gjstest/public/matchers/function_matchers_test))
+$(eval $(call add_js_test,gjstest/public/matchers/number_matchers_test))
+$(eval $(call add_js_test,gjstest/public/matchers/string_matchers_test))
