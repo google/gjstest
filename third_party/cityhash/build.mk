3b746be767849fd3620beabbfa1d3d053e746947
Aaron Jacobs <jacobsa@google.com>
2011-09-20 12:14:56 +1000

    Fixed some build errors.

--- third_party/cityhash/build.mk
+++ third_party/cityhash/build.mk
@@ -1,21 +1,21 @@
-PKG := third_party/cityhash
+CITY_PKG := third_party/cityhash
 
 ######################################################
 # Libraries
 ######################################################
 
-$(PKG)/cityhash.o : $(PKG)/city.h $(PKG)/city.cc
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(PKG)/city.cc -o $(PKG)/city.o
+$(CITY_PKG)/cityhash.o : $(CITY_PKG)/city.h $(CITY_PKG)/city.cc
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(CITY_PKG)/city.cc -o $(CITY_PKG)/city.o
 
 ######################################################
 # Tests
 ######################################################
 
 TESTS +=\
-    $(PKG)/city-test
+    $(CITY_PKG)/city-test
 
-$(PKG)/city-test.o : $(PKG)/city-test.cc $(PKG)/city.h $(PKG)/citycrc.h
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(PKG)/city-test.cc -o $(PKG)/city-test.o
+$(CITY_PKG)/city-test.o : $(CITY_PKG)/city-test.cc $(CITY_PKG)/city.h $(CITY_PKG)/citycrc.h
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(CITY_PKG)/city-test.cc -o $(CITY_PKG)/city-test.o
 
-$(PKG)/city-test : $(PKG)/cityhash.o $(PKG)/city-test.o
+$(CITY_PKG)/city-test : $(CITY_PKG)/cityhash.o $(CITY_PKG)/city-test.o
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -lpthread $^ -o $@ -lglog
