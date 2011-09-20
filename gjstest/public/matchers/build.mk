8808d0bf874172476b2831811a8a38e7281f6e40
Aaron Jacobs <jacobsa@google.com>
2011-09-20 14:32:17 +1000

    Deleted some old stuff.

--- gjstest/public/matchers/build.mk
+++ gjstest/public/matchers/build.mk
@@ -9,9 +9,3 @@ JS_TESTS +=\
 
 $(MATCHERS_PKG)/array_matchers_test : $(MATCHERS_PKG)/array_matchers_test.js $(JS_TEST_DEPS) gjstest/internal/driver/cpp/driver
 	gjstest/internal/driver/cpp/driver --js_files=`echo "$(JS_TEST_DEPS) $(MATCHERS_PKG)/array_matchers_test.js" | perl -i -pe 's: :,:g'`
-
-$(PKG)/xml_writer_test.o : $(PKG)/xml_writer_test.cc $(PKG)/xml_writer.h $(TEST_HEADERS) base/*.h
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $(PKG)/xml_writer_test.cc -o $(PKG)/xml_writer_test.o
-
-$(PKG)/xml_writer_test : $(PKG)/xml_writer.o $(PKG)/xml_writer_test.o third_party/gmock/gmock_main.a base/base.a third_party/cityhash/cityhash.o
-	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -lpthread $^ -o $@ -lxml2 -lglog
