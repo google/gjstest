60ad10ab81986a44a7f31a501f6a5116b4d371af
Aaron Jacobs <jacobsa@google.com>
2011-09-20 11:44:13 +1000

    Fixed some platform detection code.

--- base/stl_decl.h
+++ base/stl_decl.h
@@ -22,7 +22,7 @@
 #if defined(STL_MSVC)  /* If VC++'s STL */
 #include "base/stl_decl_msvc.h"
 
-#elif defined(__APPLE__) && defined(OS_MACOSX) /* gcc with a twist */
+#elif defined(__APPLE__) /* gcc with a twist */
 #include "base/stl_decl_osx.h"
 
 #elif !defined(__GNUC__)
