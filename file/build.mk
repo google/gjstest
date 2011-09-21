06865c784dca09af71b9f601f77dcbf67741fede
Aaron Jacobs <jacobsa@google.com>
2011-09-21 11:41:01 +1000

    Deleted build.mk files.

--- file/build.mk
+++
@@ -1,7 +0,0 @@
-FILE_PACKAGE = gjstest/internal/driver/cpp
-
-######################################################
-# Libraries
-######################################################
-
-$(FILE_PACKAGE)/file_utils.o : $(FILE_PACKAGE)/file_utils.h $(FILE_PACKAGE)/file_utils.cc base/*.h
--- strings/build.mk
+++
@@ -1,9 +0,0 @@
-STRINGS_PACKAGE = strings
-
-######################################################
-# Libraries
-######################################################
-
-$(STRINGS_PACKAGE)/ascii_ctype.o : $(STRINGS_PACKAGE)/ascii_ctype.cc $(STRINGS_PACKAGE)/*.h base/*.h
-
-$(STRINGS_PACKAGE)/strutil.o : $(STRINGS_PACKAGE)/strutil.cc $(STRINGS_PACKAGE)/*.h base/*.h
