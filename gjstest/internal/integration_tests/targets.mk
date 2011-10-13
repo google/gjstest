b3b49adeee27e3d846aac4fed7a45e4186da3b43
Aaron Jacobs <jacobsa@google.com>
2011-10-13 14:50:57 +1100

    Added a missing dependency.

--- gjstest/internal/integration_tests/targets.mk
+++ gjstest/internal/integration_tests/targets.mk
@@ -19,7 +19,7 @@ $(eval $(call cc_binary, \
 
 INT_TEST_ARGS =
 
-gjstest/internal/integration_tests/integration_test.out : gjstest/internal/integration_tests/integration_test.bin scripts/cc_test_run.sh share gjstest/internal/cpp/gjstest.bin
+gjstest/internal/integration_tests/integration_test.out : gjstest/internal/integration_tests/integration_test.bin scripts/cc_test_run.sh share gjstest/internal/cpp/gjstest.bin gjstest/internal/integration_tests/*.js gjstest/internal/integration_tests/*.golden.txt gjstest/internal/integration_tests/*.golden.xml
 	./scripts/cc_test_run.sh gjstest/internal/integration_tests/integration_test --test_srcdir=gjstest/internal/integration_tests --data_dir=share/gjstest --gjstest_binary=gjstest/internal/cpp/gjstest.bin $(INT_TEST_ARGS)
 
 CC_TESTS += gjstest/internal/integration_tests/integration_test.out
