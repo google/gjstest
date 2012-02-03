220f31a1960828efb8dae56b08307040daf82427
Aaron Jacobs <jacobsa@google.com>
2012-02-03 15:58:53 +1100

    Added a placeholder for a function that will implement typed arrays.

---
+++ gjstest/internal/cpp/typed_arrays.cc
@@ -0,0 +1,30 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+// Author: jacobsa@google.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gjstest/internal/cpp/typed_arrays.h"
+
+using v8::Handle;
+using v8::ObjectTemplate;
+
+namespace gjstest {
+
+void ExportTypedArrays(
+    const Handle<ObjectTemplate>& global_template);{
+  // TODO(jacobsa): Implement me.
+}
+
+}  // namespace gjstest
+
+}  // namespace gjstest
