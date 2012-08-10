e99a2806384f7f6f72c617c3f75bf64f4d5b9519
Aaron Jacobs <jacobsa@google.com>
2012-08-10 10:34:49 +1000

    Renamed test suite in preparation for adding another.

--- gjstest/internal/integration_tests/registration_test.js
+++ gjstest/internal/integration_tests/registration_test.js
@@ -15,27 +15,35 @@
 
 // A test file that makes sure test registration works as it should.
 
-function RegistrationTest() {}
-registerTestSuite(RegistrationTest);
+////////////////////////////////////////////////////////////////////////
+// Registration with helper
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// Registration without helper
+////////////////////////////////////////////////////////////////////////
+
+function BareRegistrationTest() {}
+registerTestSuite(BareRegistrationTest);
 
 // Functions without a trailing underscore should not be run as test cases.
-RegistrationTest.prototype.helperFunction_ = function() {
+BareRegistrationTest.prototype.helperFunction_ = function() {
   throw new Error('I shouldn\'t have been run!');
 };
 
 // The one special name without a trailing underscore is 'tearDown'. It should
 // be executed after each test, but not as a test case itself.
-RegistrationTest.prototype.tearDown = function() {
+BareRegistrationTest.prototype.tearDown = function() {
   // Create some output to make sure tearDown is run after each case.
   gjstest.log('tearDown has been run.');
 };
 
 // A test case with any old name should definitely be run.
-RegistrationTest.prototype.FooBar = function() {
+BareRegistrationTest.prototype.FooBar = function() {
 };
 
 // Test cases called 'constructor' should be executed, even though this is a
 // magic property automatically created on Foo.prototype for any function Foo.
-RegistrationTest.prototype.constructor = function() {
+BareRegistrationTest.prototype.constructor = function() {
   expectTrue(false);
 };
